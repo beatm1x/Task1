705fd2ddbf82139c176d0c1aeaf58e894cb48118
beatm1x <lesha0-lesha0@mail.ru>
2013-04-30 20:25:43 +0300

    05

---
+++ learn_ruby/05_silly_blocks/silly_blocks.rb
@@ -0,0 +1,11 @@
+def reverser &block
+  block.call.split(' ').map {|w| w.reverse}.join(' ')
+end
+def adder value=1,&block
+  block.call+value
+end
+def repeater value=1,&block
+  value.times do
+    block.call
+  end
+end
