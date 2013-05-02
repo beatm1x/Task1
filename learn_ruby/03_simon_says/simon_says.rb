d5e334a70629cb9d7e11cd97c78b12b8fb86c8e1
beatm1x <lesha0-lesha0@mail.ru>
2013-05-02 18:15:35 +0300

    refact code and fix in 01,02,03

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -1,32 +1,31 @@
 def echo(str)
    str
 end
+
 def shout(str)
   str.upcase
 end
-def repeat(str,n=2)
-  ((str+' ') * (n-1))+str
+
+def repeat(str, n=2)
+  ((str + ' ') * (n - 1)) + str
 end
-def start_of_word(str,n)
-  str.slice(0,n)
+
+def start_of_word(str, n)
+  str.slice(0, n)
 end
+
 def first_word(str)
-  str.slice(0,str.index(' '))
+  str.slice(0, str.index(' '))
 end
+
 def titleize(str)
-  a=['and','over','the']
-  first=true;
-  str.split(' ').map {|w|
-    little=false
-    a.each do |e|
-      if w==e
-        little=true
-      end
+  words=str.split(' ').map {|word|
+    if ['the', 'and', 'over'].include?(word)
+      word
+    else
+      word.capitalize
     end
-    if !little or first
-      w=w.capitalize
-    end
-    first=false
-    w
-  }.join(' ')
+  }
+  words.first.capitalize!
+  words.join(' ')
 end
