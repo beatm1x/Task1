878972c06e7c26246897670b3899495ae3400683
beatm1x <lesha0-lesha0@mail.ru>
2013-05-13 23:22:32 +0300

    06 fix space

--- learn_ruby/06_performance_monitor/performance_monitor.rb
+++ learn_ruby/06_performance_monitor/performance_monitor.rb
@@ -1,11 +1,11 @@
 def measure (x = 1)
-	all_time = 0
-	x.times do
-		time_begin = Time.now
-		yield
-		time_end = Time.now
+  all_time = 0
+  x.times do
+    time_begin = Time.now
+    yield
+    time_end = Time.now
     all_time += time_end - time_begin
-	end
+  end
   all_time / x
 end
   
